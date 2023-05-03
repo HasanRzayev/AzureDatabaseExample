f5ea936197f2a993dfb7d972a52cd064e6554815
HasanRzayev <hsnrz2002@gmail.com>
2023-05-03 09:43:51 +0400

    Add project files.

---
+++ AzureDatabaseExample/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using AzureDatabaseExample.data;
+using AzureDatabaseExample.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
+namespace AzureDatabaseExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly StepDataContext _dbContext;
+
+        public HomeController(StepDataContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            //var myEntities = _dbContext.ProductCategories.Select(a=>a.Name).ToList();
+            ViewBag.Categories = new SelectList(_dbContext.ProductCategories, "ProductCategoryId", "Name");
+            //ViewBag.Categories = myEntities;
+            return View("Index", null);
+        }
+        public ActionResult GetProductsByCategoryId(int categoryId)
+        {
+            var products = _dbContext.Products.Where(p => p.ProductCategoryId == categoryId).ToList();
+            return PartialView("ProductList", products);
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AzureDatabaseExample/wwwroot/js/index.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#CategoryList").change(function () {
+        var categoryId = $(this).val();
+        $.ajax({
+            type: "GET",
+            url: "@Url.Action("GetProductsByCategoryId", "Product")",
+            data: { categoryId: categoryId },
+            success: function (data) {
+                // Ürünlerin listelendiği yer
+                console.log(data)
+                $("#productList").html(data);
+            },
+            error: function () {
+                alert("Bir hata oluştu!");
+            }
+        });
+    });
+});
